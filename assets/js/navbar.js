6dbbc17438f31a85b6c5baf922f058a12c86b63e
Anders Ravn <anders@adriansen.dk>
2020-12-17 21:53:11 +0100

    Bug Fixes #1

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -5,12 +5,14 @@ function toggleMenu(){
     if(menu.style.visibility==="visible"){
         menu.style.visibility = "hidden";
         menu.classList.remove("navbar-menu-expanded");
-        footer.style.opacity = 1;
-
+        if(footer != null){
+            footer.style.opacity = 1;
+        }
     }else{
         menu.style.visibility = "visible";
         menu.classList.add("navbar-menu-expanded");
-
-        footer.style.opacity = 0;
+        if(footer != null){
+            footer.style.opacity = 0;
+        }
     }   
 }
