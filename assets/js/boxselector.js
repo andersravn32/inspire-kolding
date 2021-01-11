3240992988af651f011c4b8b07323223469b20c3
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2021-01-11 14:08:12 +0100

    Created more documents & bug fixes

--- assets/js/boxselector.js
+++ assets/js/boxselector.js
@@ -1,13 +1,28 @@
 var boxIndex = 1;
-
-class Box{
-
-}
+var endOfBoxes = 21;
 
 function addBox(){
-
+    if (boxIndex == endOfBoxes){
+        boxIndex = 1;
+        updateBox();
+        alert(boxIndex);
+    }else{
+        boxIndex++;
+        updateBox();
+    }
 }
 
 function removeBox(){
+    if(boxIndex == 1){
+        boxIndex = endOfBoxes;
+        updateBox();
+        alert(boxIndex);
+    }else{
+        boxIndex--;
+        updateBox();
+    }
+}
 
+function updateBox(){
+    
 }
